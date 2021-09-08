c0ce133b8a3bb49923cbbb24ded1b0689d6fa6da
Michel Sakr <michel.s.sakr@gmail.com>
2021-09-08 21:24:39 +0300

    11. The great outdoors

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController control;
+    public GameObject mainCamera;
+    private float moveHorizontal;
+    private float moveVertical;
+    private float speed;
+    private float jumpSpeed;
+    private float gravity;
+    private Vector3 movement = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = GetComponent<CharacterController>();
+        speed = 10f;
+        jumpSpeed = 10f;
+        gravity = 20f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = transform.position - mainCamera.transform.position;
+        relativePos.Normalize();
+        relativePos.y = 0;
+
+        if (control.isGrounded)
+        {
+            moveHorizontal = Input.GetAxisRaw("Horizontal");
+            moveVertical = Input.GetAxisRaw("Vertical");
+            movement = (moveHorizontal * mainCamera.transform.right) + (moveVertical * relativePos);
+            movement = transform.TransformDirection(movement);
+            movement.Normalize();
+            movement *= speed;
+            if (Input.GetButton("Jump"))
+                movement.y = jumpSpeed;
+        }
+        movement.y -= gravity * Time.deltaTime;
+        control.Move(movement * Time.deltaTime);
+
+        if (transform.position.y < -21)
+        {
+            transform.position = new Vector3(0f, 30f, 0f);
+            movement = Vector3.zero;
+
+        }
+    }
+}
